ccf9deb50a7ebdec9f39c53167d0b594c60b7fbe
Gustav.Nordlander <Gustav.nordlander@elev.ga.ntig.se>
2026-05-03 00:20:39 +0200

    Jag använder ju Ai för att va säker jag "" klara kriterierna utifall att jag missförstod dem. men den sa jag inte hadde två gneriska klasser så jag gjorde en till är fort farande osäker på vad som definerar en generisk klass Ai sa det var "<_>" som gjorde det fattar inte hur dock

--- Slutprojekt/klasser/Container.cs
+++ Slutprojekt/klasser/Container.cs
@@ -6,20 +6,25 @@ public class Container<T>
 
     public static void Testdemo()
     {
-        Console.WriteLine("Jag ska demonstrera en generisk klass. jag förstod det som ngn typ av klass som kan ta emot input av olika typer beronde på vad man väljer isch");
-        Console.WriteLine("i alla fall så här är två variabler från samma klass men dem håller olika typer av input int och string");
-        Console.WriteLine("man skulle nog kunna göra detta till något coolare men jag gjorde bara så här för att det var det jag förstod det som");
-        Thread.Sleep(2000);
         var nummer = new Container<int>();
         nummer.inneholl = 42;
 
         var ord = new Container<string>();
         ord.inneholl = "pappa skämt";
 
+        //lite yap om vad jag gjort
         Console.WriteLine("jag la in två inputs en siffra och en text som sparas i två variabler som kommer från den här klassen");
+        // skriver ut vad som ligger i dem variablerna jag skapade med olika innehål trots dem har "" samma orgin
         Console.WriteLine(nummer.inneholl);
         Console.WriteLine(ord.inneholl);
-        
+        Console.WriteLine("");
+        Console.WriteLine("Eller här har jag gjort en lista med en generisk klass som håller årtal som exempel");
+
+        // Hör gjorde jag en till generisk klass
+        List<string> årtal = new List<string>();
+        årtal.Add("2007");
+        årtal.Add("2006");
+
     }
 
 }
