3389271508dd8ddfc6460bde12b056b58abf8305
Gustav.Nordlander <Gustav.nordlander@elev.ga.ntig.se>
2026-05-11 15:30:26 +0200

    fixa allt nu

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,7 +1,5 @@
 ﻿
-
 Iformen f = new();
-Info F = new();
 
 // här är själva loopen för programmet Som går i ordningen.  Meny -> val -> visar dem kriterierna ->starta om loopen
 string C = "_C nivå";
@@ -17,14 +15,17 @@ while (true)
     string val = Console.ReadLine();//input för vad som ska köras
     //Så här är den underbara if-sats ormen som bestömmer vad som ska köras
     if (val == "0") { break; }
-    else if (val == "1"){f.Körareademo();}
-    else if (val == "2"){Container<string>.Testdemo();}
+    else if (val == "1") { f.Körareademo(); }
+    else if (val == "2") { Container<string>.Testdemo(); }
     else if (val == "3")
     {
-        F.Cars.Add(new Bil() { name = "Audi", hp = 400});
-        F.Motos.Add(new Motorcykel() { name = "Ninja", kubik = 200});
-    
-        Console.WriteLine(F.Motos[0].name + " " + F.Motos[0].kubik);
+        // Motorcykel motorcykel = new Kawasaki();
+        // motorcykel.Stegra();
+        Info info = new();
+        info.AddFordon();
+        info.showlist();
+        info.slangabens();
+
     }
     else
     {
--- Slutprojekt/klasser/REDO.cs
+++ Slutprojekt/klasser/REDO.cs
@@ -2,8 +2,69 @@
 
 public class Info
 {
-    public List<Bil> Cars = [];
     public List<Motorcykel> Motos = [];
+
+    public void AddFordon()
+    {
+        string val = "";
+        Console.WriteLine("Välj vilken motorcykel du vill lägga till 1(Kawasaki) 2(Äcklig Harley)");
+        while (true)
+        {
+
+            string tmp = Console.ReadLine();
+            if (tmp == "1")
+            {
+                Console.WriteLine("du valde kawasaki");
+                val = tmp;
+                break;
+            }
+            else if (tmp == "2")
+            {
+                Console.WriteLine("du valde ew harley...");
+                val = tmp;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("test3 fail");
+            }
+        }
+        if (val == "1")
+        {
+            Motos.Add(new Kawasaki() { name = "Ninja", });
+            Motos.Add(new Harley() { name = "tjockis", });
+        }
+        else if (val == "2")
+        {
+            Motos.Add(new Harley() { name = "tjockis", });
+            Motos.Add(new Kawasaki() { name = "Ninja", });
+        }
+    }
+
+    public void showlist()
+    {
+        Console.WriteLine("i din lista/garage ligger en " + Motos[0]);
+        Console.WriteLine("Den har " + Motos[0].kubik + "-kubik och är av modelen " + Motos[0].name);
+        Console.WriteLine("det ligger även en " + Motos[1]);
+        Console.WriteLine("Den har " + Motos[1].kubik + "-kubik och är av modelen " + Motos[1].name);
+    }
+
+    public void slangabens()
+    {
+        if (Motos.Count >= 2)
+        {
+            Motos[0].fuel += Motos[1].fuel;
+            Motos[1].fuel -= Motos[1].fuel;
+            Console.WriteLine("Vi slanga bensin från din första mc till andra nu är deras respektive fuellevel är " + Motos[0].fuel + " och " + Motos[1].fuel);
+            Console.WriteLine("sen ger vi tillbaka det");
+            Motos[1].fuel += Motos[0].fuel;
+            Motos[0].fuel -= Motos[0].fuel;
+            Console.WriteLine("nu är deras respektive fuellevel är " + Motos[0].fuel + " och " + Motos[1].fuel);
+
+        }
+
+
+    }
 
 }
 
@@ -21,33 +82,23 @@ public class Fordon
             if (value != "") _name = value;
         }
     }
-
-    
-    
-
-    
-}
-
-public class Bil: Fordon
-{
-
-    private int _hp;
-    public int hp
+    private int _fuel;
+    public int fuel
     {
         get
         {
-            return _hp;
+            return _fuel;
         }
         set
         {
-            if(value != 0) _hp = value;
+            if (value >= 0) _fuel = value;
         }
     }
-    
+
+
 }
 
-
-public class Motorcykel: Fordon
+public class Motorcykel : Fordon
 {
 
     private int _kubik;
@@ -59,29 +110,26 @@ public class Motorcykel: Fordon
         }
         set
         {
-            if(value != 0) _kubik = value;
+            if (value != 0) _kubik = value;
         }
     }
-
 }
 
-public class Kawasaki: Motorcykel
+public class Kawasaki : Motorcykel
 {
-    private int 
-    
+    public Kawasaki()
+    {
+        kubik = 1000;
+        fuel = 500;
+        Console.WriteLine("fuel är " + fuel);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// Mattebok mb = new();
-// mb.kapitel.Add(new Kapitel() { Name = "Geomtri" });
-// mb.kapitel[0].Name = "";
+public class Harley : Motorcykel
+{
+    public Harley()
+    {
+        kubik = 1600;
+        fuel = 1000;
+        Console.WriteLine("fuel är " + fuel);
+    }
+}
